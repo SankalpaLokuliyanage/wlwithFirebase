6d1cb551d49670d6175f6e868e76345c0e297f76
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-16 23:43:54 +0530

    add signin with firebase

--- app/src/main/java/com/example/wedlock/activities/SigninActivity.java
+++ app/src/main/java/com/example/wedlock/activities/SigninActivity.java
@@ -1,19 +1,28 @@
 package com.example.wedlock.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.wedlock.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SigninActivity extends AppCompatActivity {
 
     Button signInText;
     EditText inputEmailLogin, inputPasswordLogin;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +36,47 @@ public class SigninActivity extends AppCompatActivity {
             }
         });
 
+        auth = FirebaseAuth.getInstance();
+
         signInText = findViewById(R.id.signInText);
         inputEmailLogin = findViewById(R.id.inputEmailLogin);
         inputPasswordLogin = findViewById(R.id.inputPasswordLogin);
+
+        signInText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String userEmail = inputEmailLogin.getText().toString();
+        String userPass = inputPasswordLogin.getText().toString();
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPass)) {
+            Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        else {
+            //login user
+            auth.signInWithEmailAndPassword(userEmail, userPass)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(SigninActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(SigninActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
