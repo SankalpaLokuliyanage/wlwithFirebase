d0b8721f1b5adc06e4d527a8e224086cb15c86af
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-16 23:09:05 +0530

    add validations for signup credentials

--- app/src/main/java/com/example/wedlock/activities/SignupActivity.java
+++ app/src/main/java/com/example/wedlock/activities/SignupActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.wedlock.R;
 import com.example.wedlock.activities.SigninActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
     Button signUpText;
     EditText inputEmail, inputUsername, inputPassword, reenterPassword;
+
+    //authentication
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +34,48 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        auth = FirebaseAuth.getInstance();
+
         signUpText = findViewById(R.id.signUpText);
         inputEmail = findViewById(R.id.inputEmail);
         inputUsername = findViewById(R.id.inputUsername);
         inputPassword = findViewById(R.id.inputPassword);
         reenterPassword = findViewById(R.id.reenterPassword);
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+    }
+
+    private void createUser() {
+        String userName = inputUsername.getText().toString();
+        String userEmail = inputEmail.getText().toString();
+        String userPass = inputPassword.getText().toString();
+        String userCPass = reenterPassword.getText().toString();
+
+        if (TextUtils.isEmpty(userName)) {
+            Toast.makeText(this, "Name cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPass)) {
+            Toast.makeText(this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userCPass)) {
+            Toast.makeText(this, "Re Enter your password to confirm", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!userPass.equals(userCPass)) {
+            Toast.makeText(this, "Password not matched!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
