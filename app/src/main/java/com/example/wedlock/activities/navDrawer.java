a821761f9a672013ce7f824f796002a3047d30bf
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-17 14:31:49 +0530

    update fragements with icons with slide drawer

--- app/src/main/java/com/example/wedlock/activities/navDrawer.java
+++ app/src/main/java/com/example/wedlock/activities/navDrawer.java
@@ -30,13 +30,7 @@ public class navDrawer extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarNavDrawer.toolbar);
-        binding.appBarNavDrawer.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
