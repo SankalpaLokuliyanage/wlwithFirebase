6c4f5886069aefd18adcc335bee93d61b27b5d9e
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-18 18:24:44 +0530

    move fragements to their packages

--- app/src/main/java/com/example/wedlock/ui/cart/MyCartFragment.java
+++ app/src/main/java/com/example/wedlock/ui/cart/MyCartFragment.java
@@ -1,4 +1,4 @@
-package com.example.wedlock;
+package com.example.wedlock.ui.cart;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.wedlock.R;
 
 public class MyCartFragment extends Fragment {
 
