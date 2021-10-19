79acb4912229994c1e4027d71aefb316f2aa592a
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-19 17:01:47 +0530

    fragment cart designing

--- app/src/main/java/com/example/wedlock/activities/DetailedActivity.java
+++ app/src/main/java/com/example/wedlock/activities/DetailedActivity.java
@@ -1,38 +1,61 @@
 package com.example.wedlock.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.wedlock.R;
 import com.example.wedlock.models.viewAllModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 public class DetailedActivity extends AppCompatActivity {
 
+    int totalQuantity = 1;
+    int totalPrice = 0;
     ImageView detailedImg;
     TextView name, price, description;
     Button addToCart;
     viewAllModel viewAllModel = null;
 
+    TextView quantity;
+    ImageView addItem, removeItem;
+
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
 
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
         final Object object = getIntent().getSerializableExtra("detail");
 
         if (object instanceof viewAllModel) {
             viewAllModel = (viewAllModel) object;
         }
+
+        addItem = findViewById(R.id.add_item);
+        removeItem = findViewById(R.id.remove_item);
+        quantity = findViewById(R.id.quantity);
 
         detailedImg = findViewById(R.id.detailed_img);
         name = findViewById(R.id.detailed_name);
@@ -44,11 +67,79 @@ public class DetailedActivity extends AppCompatActivity {
             name.setText(viewAllModel.getName());
             description.setText(viewAllModel.getDescription());
             price.setText("Price: LKR. "+ viewAllModel.getPrice());
+
+            int gotPrice = Integer.parseInt(viewAllModel.getPrice());
+
+            totalPrice = gotPrice * totalQuantity;
         }
 
 
         addToCart = findViewById(R.id.add_to_cart);
 
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addedToCart();
+            }
+        });
+
+
+        addItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalQuantity < 10) {
+                    totalQuantity++;
+                    quantity.setText(String.valueOf(totalQuantity));
+                    int gotPrice = Integer.parseInt(viewAllModel.getPrice());
+
+                    totalPrice = gotPrice * totalQuantity;
+                }
+            }
+        });
+
+        removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (totalQuantity > 1) {
+                    totalQuantity--;
+                    quantity.setText(String.valueOf(totalQuantity));
+                    int gotPrice = Integer.parseInt(viewAllModel.getPrice());
+
+                    totalPrice = gotPrice * totalQuantity;
+
+                }
+            }
+        });
+
+    }
+
+    private void addedToCart() {
+        String saveCurrentDate, saveCurrentTime;
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+
+        cartMap.put("productName", viewAllModel.getName());
+        cartMap.put("productPrice", price.getText().toString());
+        cartMap.put("currentDate", saveCurrentDate);
+        cartMap.put("currentTime", saveCurrentTime);
+        cartMap.put("totalQuantity", quantity.getText().toString());
+        cartMap.put("totalPrice", totalPrice);
+
+        firestore.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                .collection("CurrentUser").add(cartMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentReference> task) {
+                Toast.makeText(DetailedActivity.this, "Added to cart", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
 
     }
 }
